44b5310da1d874a04259dcf621e9731a71252bf6
daif <mohamed.daif9@hotmail.com>
2023-01-16 18:50:28 +0200

    limit image uploaded size

--- gallary/src/main/java/com/example/gallary/controller/MainController.java
+++ gallary/src/main/java/com/example/gallary/controller/MainController.java
@@ -9,13 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ModelAndViewDefiningException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.gallary.entity.Pictures;
 import com.example.gallary.entity.Users;
@@ -23,6 +27,7 @@ import com.example.gallary.repository.PictureRepository;
 import com.example.gallary.repository.UserRepository;
 
 @Controller
+@ControllerAdvice
 public class MainController {
 
 	@Autowired
@@ -65,6 +70,12 @@ public class MainController {
 			FileUploadController.saveFile(upload, fileName, multipartFile);
 		}
 		return "redirect:/index";
+	}
+	
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public String handleFileUploadError(RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("error", "you couldn't upload file bigger than 2 MB");
+		return "redirect:/";
 	}
 
 }
